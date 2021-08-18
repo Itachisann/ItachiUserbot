bd7fbacf80dac9672c3c8d38e785917f432f213b
Itachisann <88840797+Itachisann@users.noreply.github.com>
2021-08-18 16:11:53 +0200

    Update anilist.py

--- userbot/plugins/anilist.py
+++ userbot/plugins/anilist.py
@@ -215,70 +215,75 @@ async def manga(event: NewMessage.Event) -> None:
     translator = Translator()
     if event.fwd_from:
         return
-    search = event.matches[0].group(1)
-    reply_to_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
-    variables = {"search": search}
-    json = (
-        requests.post(url, json={"query": manga_query, "variables": variables})
-        .json()["data"]
-        .get("Media", None)
-    )
-    ms_g = ""
-    if json:
-        title, title_native = json["title"].get("romaji", False), json["title"].get(
-            "native", False
+    match = event.matches[0].group(1)
+    if match:
+        await event.edit("__Sto cercando..attendi qualche istante...__")
+        reply_to_id = event.message.id
+        if event.reply_to_msg_id:
+            reply_to_id = event.reply_to_msg_id
+        variables = {"search": match}
+        json = (
+            requests.post(url, json={"query": manga_query, "variables": variables})
+            .json()["data"]
+            .get("Media", None)
         )
-        start_date, status, score = (
-            json["startDate"].get("year", False),
-            json.get("status", False),
-            json.get("averageScore", False),
-        )
-        if title:
-            ms_g += f"**{title}**"
-            if title_native:
-                ms_g += f" (`{title_native}`)"
-        if start_date:
-            ms_g += f"\n\n**Anno di inizio** - `{start_date}`"
-        if status:
-            stato = translator.translate(status, dest='it')
-            stato = stato.text
-            stato = stato.capitalize()
-            ms_g += f"\n**Stato** - `{stato}`"
-        if score:
-            ms_g += f"\n**Punteggio** - `{score}%`"
-        ms_g += "\n**Genere** - "
-        for x in json.get("genres", []):
-            genere = translator.translate(x, dest='it')
-            genere = genere.text
-            ms_g += f"`{genere}` - "
-        ms_g = ms_g[:-2]
-        image = json.get("bannerImage", False)
-        descrizione = translator.translate(
-            json.get('description', None), dest='it')
-        descrizione = descrizione.text
-        ms_g += f"\n\n__{descrizione}__"
-        ms_g = (
-            ms_g.replace("<br>", "\n")
-            .replace("</br>", "")
-            .replace("<i>", "")
-            .replace("</i>", "")
-        )
-        if image:
-            try:
-                await event.client.send_file(
-                    event.chat_id,
-                    image,
-                    caption=ms_g,
-                    parse_mode="md",
-                    reply_to=reply_to_id,
-                )
-                await event.delete()
-            except BaseException:
-                pass
-        else:
-            await event.edit(ms_g)
+        ms_g = ""
+        if json:
+            title, title_native = json["title"].get("romaji", False), json["title"].get(
+                "native", False
+            )
+            start_date, status, score = (
+                json["startDate"].get("year", False),
+                json.get("status", False),
+                json.get("averageScore", False),
+            )
+            if title:
+                ms_g += f"**{title}**"
+                if title_native:
+                    ms_g += f" (`{title_native}`)"
+            if start_date:
+                ms_g += f"\n\n**Anno di inizio** - `{start_date}`"
+            if status:
+                stato = translator.translate(status, dest='it')
+                stato = stato.text
+                stato = stato.capitalize()
+                ms_g += f"\n**Stato** - `{stato}`"
+            if score:
+                ms_g += f"\n**Punteggio** - `{score}%`"
+            ms_g += "\n**Genere** - "
+            for x in json.get("genres", []):
+                genere = translator.translate(x, dest='it')
+                genere = genere.text
+                ms_g += f"`{genere}` - "
+            ms_g = ms_g[:-2]
+            image = json.get("bannerImage", False)
+            descrizione = translator.translate(
+                json.get('description', None), dest='it')
+            descrizione = descrizione.text
+            ms_g += f"\n\n__{descrizione}__"
+            ms_g = (
+                ms_g.replace("<br>", "\n")
+                .replace("</br>", "")
+                .replace("<i>", "")
+                .replace("</i>", "")
+            )
+            await event.edit("__Dati raccolti..Sto costruendo il messaggio__")
+            if image:
+                try:
+                    await event.client.send_file(
+                        event.chat_id,
+                        image,
+                        caption=ms_g,
+                        parse_mode="md",
+                        reply_to=reply_to_id,
+                    )
+                    await event.delete()
+                except BaseException:
+                    pass
+            else:
+                await event.edit(ms_g)
+    else:
+        await event.edit("__Devi inserire il nome di un manga!__")
 
 
 @client.createCommand(
