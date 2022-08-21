30513c945f6fadb879d0ca8bf10107162eb02122
Itachisann <vito.russi2007@gmail.com>
2022-08-21 22:23:36 +0200

    add AI chatbot

--- userbot/plugins/functions/functions.py
+++ userbot/plugins/functions/functions.py
@@ -148,3 +148,37 @@ async def formatJSON(outData, search):
     cat = f"{descrizione}"
     msg += "__" + re.sub("<br>", "\n", cat) + "__"
     return msg
+
+
+async def async_searcher(
+    url: str,
+    post: bool = None,
+    headers: dict = None,
+    params: dict = None,
+    json: dict = None,
+    data: dict = None,
+    ssl=None,
+    re_json: bool = False,
+    re_content: bool = False,
+    real: bool = False,
+    *args,
+    **kwargs,
+):
+    try:
+        import aiohttp
+    except ImportError:
+        return
+    async with aiohttp.ClientSession(headers=headers) as client:
+        if post:
+            data = await client.post(
+                url, json=json, data=data, ssl=ssl, *args, **kwargs
+            )
+        else:
+            data = await client.get(url, params=params, ssl=ssl, *args, **kwargs)
+        if re_json:
+            return await data.json()
+        if re_content:
+            return await data.read()
+        if real:
+            return data
+        return await data.text()
